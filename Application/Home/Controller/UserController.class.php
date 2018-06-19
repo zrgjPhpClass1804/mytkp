d7cadbe1d3659a3d40c6fe5cccbb113dbc120528
wuchangyong <9578987@qq.com>
2018-06-19 14:02:52 +0800

    验证码

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -2,45 +2,64 @@
 namespace Home\Controller;
 use Think\Controller;
 use Think\Upload;
+use Think\Verify;
 class UserController extends Controller{
 
     private $model;
+    private $verify;
 
     public function __construct(){
         $this->model = M("tb_user");
+        $this->verify = new Verify(array(
+            'imageW' => 100,
+            'imageH' => 36,
+            'fontSize'=> 14,
+            'useCurve'=> true,
+            'useNoise'=> false,
+            'length' => 4,
+            'codeSet'=> '0123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'
+        ));
     }
 
     /**
      * 用户登录
      */
-    public function login($userName=null, $userPass=null){
+    public function login($userName=null, $userPass=null, $vcode=""){
         //$userName = $_POST["userName"];
         //$userPass = $_POST["userPass"];
         //$userName = I("userName");
         //echo "$userName, $userPass";
-        $data = $this->model->where("userName='%s'", $userName)->select();
-        if(count($data) > 0){
-            if($data[0]["userpass"] == $userPass){
-                session("user", $data[0]);
-                /**
-                 * 权限管理-利用当前登录用户id去查询该用户拥有的菜单权限
-                 */ 
-                $sql = "select distinct m.* from tb_userrole ur, tb_rolemenu rm, tb_menu m where ur.userid=%d and ur.roleid=rm.roleid and rm.menuid=m.mid and m.isHomePage=1";
-                $menus = $this->model->query($sql, $data[0]["userid"]);
-                session("menus", $menus);
-                header("location:".ROOT."Public/bootstrap/admin.php");
-                //$this->redirect("/Home/User/jump");
-                //redirect("/tp2/index.php/Home/User/jump");
+        $v = $this->verify->check($vcode, "");
+        if($v){
+            $data = $this->model->where("userName='%s'", $userName)->select();
+            if(count($data) > 0){
+                if($data[0]["userpass"] == $userPass){
+                    session("user", $data[0]);
+                    /**
+                     * 权限管理-利用当前登录用户id去查询该用户拥有的菜单权限
+                     */ 
+                    $sql = "select distinct m.* from tb_userrole ur, tb_rolemenu rm, tb_menu m where ur.userid=%d and ur.roleid=rm.roleid and rm.menuid=m.mid and m.isHomePage=1";
+                    $menus = $this->model->query($sql, $data[0]["userid"]);
+                    session("menus", $menus);
+                    header("location:".ROOT."Public/bootstrap/admin.php");
+                    //$this->redirect("/Home/User/jump");
+                    //redirect("/tp2/index.php/Home/User/jump");
+                }else{
+                    //密码错误
+                    session("msg", 2);
+                    header("location:".ROOT."login.php");
+                }
             }else{
-                $msg = 2;//密码错误
-                session("msg", $msg);
+                //用户名不存在
+                session("msg", 3);
                 header("location:".ROOT."login.php");
             }
         }else{
-            $msg = 3;//用户名不存在
-            session("msg", $msg);
+            //用户名不存在
+            session("msg", 4);
             header("location:".ROOT."login.php");
         }
+        
     }
 
     public function setHeadPicture(){
@@ -71,6 +90,10 @@ class UserController extends Controller{
     public function logout(){
         session_destroy();
         header("location:".ROOT."login.php");
+    }
+    
+    public function getVcode(){
+        $this->verify->entry();
     }
 
 }
